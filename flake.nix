a0ee1fe054b90e348ed817aff3209b3ad33b46ca
Isabel <isabel@isabelroses.com>
2024-05-25 18:00:02 +0100

    feat: nix flake (#1)

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: function nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./nix/shell.nix { };
+      });
+
+      packages = forAllSystems (pkgs: rec {
+        default = purr;
+        purr = pkgs.callPackage ./nix/default.nix { };
+      });
+    };
+}
---
+++ nix/default.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  rustPlatform,
+  pkg-config,
+  openssl,
+  stdenv,
+  darwin,
+}:
+rustPlatform.buildRustPackage {
+  pname = "purr";
+  inherit ((lib.importTOML ../Cargo.toml).package) version;
+
+  src = ../.;
+  cargoLock.lockFile = ../Cargo.lock;
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [ openssl ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
+
+  meta = with lib; {
+    description = "Utility commands for managing userstyles";
+    homepage = "https://github.com/uncenter/purr";
+    license = licenses.mit;
+    mainProgram = "purr";
+  };
+}
