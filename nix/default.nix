27d755480c6498c9d093cbec8dd10817e3267c25
uncenter <47499684+uncenter@users.noreply.github.com>
2024-05-25 13:46:30 -0400

    fix(nix): add SystemConfiguration build input

--- nix/default.nix
+++ nix/default.nix
@@ -15,7 +15,15 @@ rustPlatform.buildRustPackage {
 
   nativeBuildInputs = [ pkg-config ];
 
-  buildInputs = [ openssl ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
+  buildInputs =
+    [ openssl ]
+    ++ lib.optionals stdenv.isDarwin (
+      with darwin.apple_sdk.frameworks;
+      [
+        Security
+        SystemConfiguration
+      ]
+    );
 
   meta = with lib; {
     description = "Utility commands for managing userstyles";
