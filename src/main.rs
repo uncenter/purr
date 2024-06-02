a1acc6a41bc9256c0dd06a48b0aa55f4167692ef
uncenter <47499684+uncenter@users.noreply.github.com>
2024-06-02 13:48:47 -0400

    fix: adjust log level

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use color_eyre::eyre::Result;
 fn main() -> Result<()> {
 	color_eyre::install()?;
 	pretty_env_logger::formatted_builder()
-		.filter_level(log::LevelFilter::max())
+		.filter_level(log::LevelFilter::Warn)
 		.init();
 
 	let args = Cli::parse();
